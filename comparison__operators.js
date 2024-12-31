40316387f4e258285d5a24858d5d0a564fcb9c42
Touhiduzzaman Pavel <75662120+TouhidPavel@users.noreply.github.com>
2024-12-31 21:59:02 +0600

    Comparison Operators

---
+++ comparison__operators.js
@@ -0,0 +1,27 @@
+/*
+MongoDB provides several comparison operators that allow you to filter and query documents based on specific conditions.
+
+1. $eq: Matches values that are equal to a specified value.
+    Syntax: { field: { $eq: value } }
+
+2. $ne: Matches values that are not equal to a specified value.
+    Syntax: { field: { $ne: value } }
+
+3. $gt: Matches values greater than a specified value.
+    Syntax: { field: { $gt: value } }
+
+4. $gte: Matches values greater than or equal to a specified value.
+    Syntax: { field: { $gte: value } }
+
+5. $lt: Matches values less than a specified value.
+    Syntax: { field: { $lt: value } }
+
+6. $lte: Matches values less than or equal to a specified value.
+    Syntax: { field: { $lte: value } }
+
+7. $in: Matches any of the values specified in an array.
+    Syntax: { field: { $in: [value1, value2, ...] } }
+
+8. $nin: Matches none of the values specified in an array.
+    Syntax: { field: { $nin: [value1, value2, ...] } }
+*/
